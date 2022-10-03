2319e921807e4d0c71231c8d910c4d3a5eedc550
ajinsathe <110330179+ajinsathe@users.noreply.github.com>
2022-10-03 17:10:11 +0530

    Add files via upload

---
+++ Day 1 (September 03, 2022 Sat).py
@@ -0,0 +1,492 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welcome to Python
+
+# Learning Python Basics
+
+# Working with Operators
+
+# In[1]:
+
+
+1+1
+
+
+# In[2]:
+
+
+2-1
+
+
+# In[3]:
+
+
+5-2
+
+
+# In[4]:
+
+
+3*2
+
+
+# In[5]:
+
+
+6*6
+
+
+# In[6]:
+
+
+10/2
+
+
+# Floor Division
+
+# In[7]:
+
+
+10//2
+
+
+# In[8]:
+
+
+8//2
+
+
+# In[9]:
+
+
+9//2
+
+
+# In[10]:
+
+
+9/2
+
+
+# Modulus Operator to find remainder
+
+# In[11]:
+
+
+10%2
+
+
+# In[12]:
+
+
+11%2
+
+
+# Print Command "" and '' both can be used to print any statement
+
+# In[13]:
+
+
+print("Hello")
+
+
+# In[14]:
+
+
+print('Hello')
+
+
+# In[15]:
+
+
+print("We are learning Python Basics")
+
+
+# In[16]:
+
+
+print('Python is a very easy language.')
+
+
+# Variables are containers that will hold the value.
+
+# In[28]:
+
+
+a=40
+
+
+# In[29]:
+
+
+a
+
+
+# In[30]:
+
+
+print(a)
+
+
+# In[31]:
+
+
+b=5
+
+
+# In[32]:
+
+
+b
+
+
+# In[33]:
+
+
+print(b)
+
+
+# In[34]:
+
+
+a+b
+
+
+# In[35]:
+
+
+print(a+b)
+
+
+# In[36]:
+
+
+a-b
+
+
+# In[37]:
+
+
+a*b
+
+
+# In[38]:
+
+
+a/b
+
+
+# Floor division
+
+# In[39]:
+
+
+a//b
+
+
+# Modulus Operation
+
+# In[40]:
+
+
+a%b
+
+
+# In[41]:
+
+
+c=a+b
+
+
+# In[42]:
+
+
+c
+
+
+# In[43]:
+
+
+d=a-b
+
+
+# In[44]:
+
+
+d
+
+
+# In[45]:
+
+
+e=a*b
+
+
+# In[46]:
+
+
+e
+
+
+# In[47]:
+
+
+f=a/b
+
+
+# In[48]:
+
+
+f
+
+
+# In[49]:
+
+
+print(c)
+
+
+# In[50]:
+
+
+print(d)
+
+
+# In[51]:
+
+
+print(e)
+
+
+# In[54]:
+
+
+print("c=", c)
+
+
+# Power In Python
+
+# In[55]:
+
+
+2**2
+
+
+# In[56]:
+
+
+3**3
+
+
+# In[57]:
+
+
+3**2
+
+
+# In[58]:
+
+
+4**4
+
+
+# In[59]:
+
+
+2**3
+
+
+# In[60]:
+
+
+3**2
+
+
+# In[61]:
+
+
+3**6
+
+
+# In[63]:
+
+
+5**3
+
+
+# In[64]:
+
+
+4**3
+
+
+# In[65]:
+
+
+7**3
+
+
+# In[66]:
+
+
+city="London"
+
+
+# In[67]:
+
+
+city
+
+
+# In[68]:
+
+
+len(city)
+
+
+# In[69]:
+
+
+len("city")
+
+
+# In[70]:
+
+
+len("Pune")
+
+
+# In[71]:
+
+
+len("mumbai")
+
+
+# In[72]:
+
+
+len("Azerbaijan")
+
+
+# In[73]:
+
+
+city
+
+
+# In[74]:
+
+
+City
+
+
+# Python is case sensitive
+
+# In[75]:
+
+
+len(city)
+
+
+# In[76]:
+
+
+state="Uttar Pradesh"
+
+
+# In[77]:
+
+
+state
+
+
+# In[78]:
+
+
+len(state)
+
+
+# In[79]:
+
+
+100<600
+
+
+# In[80]:
+
+
+45<90
+
+
+# In[81]:
+
+
+800>400
+
+
+# In[82]:
+
+
+80<22
+
+
+# In[83]:
+
+
+45>87
+
+
+# In[84]:
+
+
+45<87
+
+
+# In[85]:
+
+
+89==89
+
+
+# In[86]:
+
+
+55==55
+
+
+# In[87]:
+
+
+55!=55
+
+
+# In[88]:
+
+
+22!=90
+
+
+# In[89]:
+
+
+22==90
+
+
+# In[90]:
+
+
+78!=30
+
+
+# In[91]:
+
+
+45==56
+
+
+# In[ ]:
+
+
+
+
